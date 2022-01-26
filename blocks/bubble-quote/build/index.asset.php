aed50c954e48ca31ea10408576369e379cee7a40
CandaceJohnsonDesigns <candace@candacejohnsondesigns.com>
2022-01-26 15:24:33 -0800

    Fixed margin with stretched column parent

--- blocks/bubble-quote/build/index.asset.php
+++ blocks/bubble-quote/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '5b8e132a6ac9e731edf806669a8cf8c3');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '316c7073fb34b67d28e4714c084adc8e');
